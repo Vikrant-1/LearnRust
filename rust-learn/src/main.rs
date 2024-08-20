d1c8c79d23d6c34a57aa66c90fe97b26f0c7910c
Vikrant <86877242+Vikrant-1@users.noreply.github.com>
2024-08-20 15:31:53 +0000

    data types in rust

--- rust-learn/src/main.rs
+++ rust-learn/src/main.rs
@@ -1,46 +1,82 @@
-// numbers
-// fn main() {
-//     let x: i32 = 5;
-//     let y: u32 = 1000;
-//     let z: f32 = 1000.001;
+// Primitive Data Type 
+// Primitive are Two types
 
-//     print!("x:{}, y:{}, z:{}", x, y, z);
+
+// Scalar
+// fn main(){
+//     println!("Hello world");
+//     // let x :i32 = 2;
+
+//     // integers types (store signed integers)
+//     // i8 , 
+//     // i16 , 
+//     // i32 , 
+//     // i64  , 
+//     // i128
+
+//     // // for unsigned we use  
+//     // u8,
+
+//     // floating point values
+
+//     // f32 , f64,
+
+//     // let floating_point :f32 = 10.9;  // default is f32 in float value
+
+
+//     // // boole
+//     // let true_or_false:bool = true  // we can also say that true is 1 and false is 0
+
+
+//     // let letter:char = 'v'; //  only contain single and only use single quotes
+
 // }
 
-// boolean
-// fn main() {
-//     let is_male: bool = false;
-//     let is_above_18: bool = true;
 
-//     if is_male {
-//         print!("you are a male");
-//     } else {
-//         print!("You are not a male");
-//     }
+// Compound
+// fn main(){
+//     // tuple -> excess element , change element
 
-//     if is_male && is_above_18 {
-//         print!("You are a legal male");
-//     }
+//     // let tup :(i32 , bool ,char) = (1 , true , 's');
+//     // excess element from tuple
+//     // println!("{} , {} , {}",tup.0 , tup.1 , tup.2);
+
+//     // let make it mut and let change it
+//     // let mut _tup :(i32 , bool ,char) = (1 , true , 's');
+//     // // _tup.0 = 10;
+//     // _tup = (10 , false , 'k');
+//     // println!("{} , {} , {}",_tup.0 , _tup.1 , _tup.2);
 // }
 
-// fn main() {
-//     let is_male: bool = false;
-//     let is_above_18: bool = true;
+use std::io;
 
-//     if is_male {
-//         print!("you are a male");
-//     } else {
-//         print!("You are not a male");
-//     }
+fn main(){
+    // array
+    // let mut arr = [1,2,3,4,5];
+    // arr[4] = 10;j
+    // println!("{}",arr[4]);
 
-//     if is_male && is_above_18 {
-//         print!("You are a legal male");
-//     }
-// }
 
-// string
-fn main() {
-    let x = String::from("my name is vikrant"); 
-    let char1 = x.chars().nth(10);
-    print!("{}",char1.unwrap());
+    // // type in array
+    // let mut arr2: [i32; 5] = [1,2,3,4,5];
+
+    // println!("{}",arr[4]);
+
+
+    // sum
+    let sum = 5 +10;
+    println!("{}",sum);
+
+    let mut num = String::new(); 
+    io::stdin()
+    .read_line(&mut num)
+    .expect("faled to read line");
+
+
+    let num:u32 = num.trim().parse().expect("Please enter a number");
+
+    // subtraction
+    let subtraction :u32 = 10 - num;
+    println!("{}",subtraction);
 }
+
