870fb0451ccf07bc01cbab254f9395804c6b2ff9
Vikrant <86877242+Vikrant-1@users.noreply.github.com>
2024-08-21 17:11:06 +0000

    main file import

--- rust-learn/src/main.rs
+++ rust-learn/src/main.rs
@@ -1,46 +1,5 @@
-// numbers
-// fn main() {
-//     let x: i32 = 5;
-//     let y: u32 = 1000;
-//     let z: f32 = 1000.001;
+mod factorial_calculator;
 
-//     print!("x:{}, y:{}, z:{}", x, y, z);
-// }
-
-// boolean
-// fn main() {
-//     let is_male: bool = false;
-//     let is_above_18: bool = true;
-
-//     if is_male {
-//         print!("you are a male");
-//     } else {
-//         print!("You are not a male");
-//     }
-
-//     if is_male && is_above_18 {
-//         print!("You are a legal male");
-//     }
-// }
-
-// fn main() {
-//     let is_male: bool = false;
-//     let is_above_18: bool = true;
-
-//     if is_male {
-//         print!("you are a male");
-//     } else {
-//         print!("You are not a male");
-//     }
-
-//     if is_male && is_above_18 {
-//         print!("You are a legal male");
-//     }
-// }
-
-// string
-fn main() {
-    let x = String::from("my name is vikrant"); 
-    let char1 = x.chars().nth(10);
-    print!("{}",char1.unwrap());
+fn main(){
+    factorial_calculator::calculate_factorial();
 }
